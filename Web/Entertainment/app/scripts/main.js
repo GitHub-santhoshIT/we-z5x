2d0a8ed494dc35921017c313223c32f2c69d4e8c
Ravirala Leela Hari Prasad <hari.prasad@zee.esselgroup.com>
2020-09-15 20:29:12 +0530

    banner added

--- Web/Entertainment/app/scripts/main.js
+++ Web/Entertainment/app/scripts/main.js
@@ -8,13 +8,29 @@ console.log('\'Allo \'Allo!');
 // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
 // $(function () { $('[data-toggle="popover"]').popover(); });
 
-
+///////////////// fixed menu on scroll for desktop
+if ($(window).width() > 992) {
+  $(window).scroll(function(){  
+     if ($(this).scrollTop() > 40) {
+        $('#navbar_top').addClass("fixed-top");
+        // add padding top to show content behind navbar
+        $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
+      }else{
+        $('#navbar_top').removeClass("fixed-top");
+         // remove padding top from body
+        $('body').css('padding-top', '0');
+      }   
+  });
+} // end if
 $('.home-block-carousel').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    dots:true,
-    autoplaySpeed: 10000,
+  dots: false,
+  infinite: true,
+  fade: true,
+  autoplay: true,
+autoplaySpeed: 5000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true
   });
 
   $('.management-talk-carousel').slick({
