31dfd069dd91ecec9fea1fc48c041da0fe0ab765
swetha <swetha.kankipati@zee.esselgroup.com>
2020-09-16 08:56:50 -0700

    backgroundchange

--- Web/Entertainment/app/scripts/main.js
+++ Web/Entertainment/app/scripts/main.js
@@ -242,6 +242,29 @@ easyScrollDots({
   'fixedNavId': '',
   'fixedNavUpward': false
 });
-          
+
+/**
+ * This was built using the scrollie jQuery Plugin
+ * https://github.com/Funsella/jquery-scrollie
+ */
+
+
+$( window ).ready(function() {
   
+  var wHeight = $(window).height();
+
+  $('.slide')
+    .height(wHeight)
+    .scrollie({
+      scrollOffset : -50,
+      scrollingInView : function(elem) {
+                 
+        var bgColor = elem.data('background');
+        
+        $('body').css('background-color', bgColor);
+        
+      }
+    });
+
+});
           
