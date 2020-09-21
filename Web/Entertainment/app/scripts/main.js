ba3ae72ac2cb423bdd89058f03677a25013159fc
Ravirala Leela Hari Prasad <hari.prasad@zee.esselgroup.com>
2020-09-21 18:46:37 +0530

    media pages added

--- Web/Entertainment/app/scripts/main.js
+++ Web/Entertainment/app/scripts/main.js
@@ -470,3 +470,13 @@ $('#myCarousel').on('click', '.nav a', function() {
 
 
 
+
+var xxxx = document.querySelector('.xxxx');
+var origOffsetY = header.offsetTop;
+
+function onScroll(e) {
+  window.scrollY >= origOffsetY ? header.classList.add('sticky') :
+                                  header.classList.remove('sticky');
+}
+
+document.addEventListener('scroll', onScroll);
