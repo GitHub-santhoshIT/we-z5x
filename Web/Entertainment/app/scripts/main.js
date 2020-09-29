a8f6d160685dd173ab2ea93df9385c9ea7d2ad8c
Ravirala Leela Hari Prasad <hari.prasad@zee.esselgroup.com>
2020-09-29 14:10:00 +0530

    new build

--- Web/Entertainment/app/scripts/main.js
+++ Web/Entertainment/app/scripts/main.js
@@ -242,19 +242,19 @@ easyScrollDots({
 });
 
 /*scroll to arrow*/
-//     $(document).ready(function(){
-//   $('a').on('click', function(event) {
-//     if (this.hash !== '') {
-//       event.preventDefault();
-//       var hash = this.hash;
-//       $('html, body').animate({
-//         scrollTop: $(hash).offset().top
-//       }, 800, function(){
-//         window.location.hash = hash;
-//       });
-//     } 
-//   });
-// });
+    $(document).ready(function(){
+  $('a').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 768, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
 
 
 
