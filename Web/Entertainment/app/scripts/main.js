8514c7db3ef0187ff5a61590c7a470f5736ad97a
Ravirala Leela Hari Prasad <hari.prasad@zee.esselgroup.com>
2020-10-05 16:12:59 +0530

    fresh build for ios test

--- Web/Entertainment/app/scripts/main.js
+++ Web/Entertainment/app/scripts/main.js
@@ -1,5 +1,6 @@
 console.log('\'Allo \'Allo!');
 
+
 $(document).ready(function(evt) {
 	var $el = $('.newcount'),
 		value = 1.3;
@@ -27,6 +28,19 @@ $(document).ready(function(evt) {
 		});
 });
 
+$(document).ready(function() {
+	if ($(window).width() > 1024) {
+		// Mouse over
+		$('body').on('mouseover', '.dropdown', function(e) {
+			$(this).children('.dropdown-toggle').dropdown('show');
+		});
+		// Mouse leave
+		$('body').on('mouseleave', '.dropdown', function(e) {
+			$(this).children('.dropdown-toggle').dropdown('hide');
+		});
+	}
+});
+
 if ($(window).width() > 992) {
 	$(window).scroll(function() {
 		if ($(this).scrollTop() > 40) {
@@ -40,6 +54,152 @@ if ($(window).width() > 992) {
 		}
 	});
 } // end if
+var slideWrapper = $('.main-slider'),
+	iframes = slideWrapper.find('.embed-player'),
+	lazyImages = slideWrapper.find('.slide-image'),
+	lazyCounter = 0;
+
+// POST commands to YouTube or Vimeo API
+function postMessageToPlayer(player, command) {
+	if (player == null || command == null) return;
+	player.contentWindow.postMessage(JSON.stringify(command), '*');
+}
+
+// When the slide is changing
+function playPauseVideo(slick, control) {
+	var currentSlide, slideType, startTime, player, video;
+
+	currentSlide = slick.find('.slick-current');
+	slideType = currentSlide.attr('class').split(' ')[1];
+	player = currentSlide.find('iframe').get(0);
+	startTime = currentSlide.data('video-start');
+
+	if (slideType === 'vimeo') {
+		switch (control) {
+			case 'play':
+				if (startTime != null && startTime > 0 && !currentSlide.hasClass('started')) {
+					currentSlide.addClass('started');
+					postMessageToPlayer(player, {
+						method: 'setCurrentTime',
+						value: startTime
+					});
+				}
+				postMessageToPlayer(player, {
+					method: 'play',
+					value: 1
+				});
+				break;
+			case 'pause':
+				postMessageToPlayer(player, {
+					method: 'pause',
+					value: 1
+				});
+				break;
+		}
+	} else if (slideType === 'youtube') {
+		switch (control) {
+			case 'play':
+				postMessageToPlayer(player, {
+					event: 'command',
+					func: 'mute'
+				});
+				postMessageToPlayer(player, {
+					event: 'command',
+					func: 'playVideo'
+				});
+				break;
+			case 'pause':
+				postMessageToPlayer(player, {
+					event: 'command',
+					func: 'pauseVideo'
+				});
+				break;
+		}
+	} else if (slideType === 'video') {
+		video = currentSlide.children('video').get(0);
+		if (video != null) {
+			if (control === 'play') {
+				video.play();
+			} else {
+				video.pause();
+			}
+		}
+	}
+}
+
+// Resize player
+function resizePlayer(iframes, ratio) {
+	if (!iframes[0]) return;
+	var win = $('.main-slider'),
+		width = win.width(),
+		playerWidth,
+		height = win.height(),
+		playerHeight,
+		ratio = ratio || 16 / 9;
+
+	iframes.each(function() {
+		var current = $(this);
+		if (width / ratio < height) {
+			playerWidth = Math.ceil(height * ratio);
+			current.width(playerWidth).height(height).css({
+				left: (width - playerWidth) / 2,
+				top: 0
+			});
+		} else {
+			playerHeight = Math.ceil(width / ratio);
+			current.width(width).height(playerHeight).css({
+				left: 0,
+				top: (height - playerHeight) / 2
+			});
+		}
+	});
+}
+
+// DOM Ready
+$(function() {
+	// Initialize
+	slideWrapper.on('init', function(slick) {
+		slick = $(slick.currentTarget);
+		setTimeout(function() {
+			playPauseVideo(slick, 'play');
+		}, 1000);
+		resizePlayer(iframes, 16 / 9);
+	});
+	slideWrapper.on('beforeChange', function(event, slick) {
+		slick = $(slick.$slider);
+		playPauseVideo(slick, 'pause');
+	});
+	slideWrapper.on('afterChange', function(event, slick) {
+		slick = $(slick.$slider);
+		playPauseVideo(slick, 'play');
+	});
+	slideWrapper.on('lazyLoaded', function(event, slick, image, imageSource) {
+		lazyCounter++;
+		if (lazyCounter === lazyImages.length) {
+			lazyImages.addClass('show');
+			// slideWrapper.slick("slickPlay");
+		}
+	});
+
+	//start the slider
+	slideWrapper.slick({
+		// fade:true,
+		autoplaySpeed: 4000,
+		lazyLoad: 'progressive',
+		speed: 600,
+		arrows: false,
+		dots: true,
+		cssEase: 'cubic-bezier(0.87, 0.03, 0.41, 0.9)'
+	});
+});
+
+// Resize event
+$(window).on('resize.slickVideoPlayer', function() {
+	resizePlayer(iframes, 16 / 9);
+});
+
+///video player
+//video player-x
 
 $('.home-block-carousel').slick({
 	infinite: true,
@@ -530,15 +690,27 @@ $(document).ready(function() {
 		});
 });
 
-$('.container').stickem({
-	item: '.stickem',
-	container: '.stickem-container',
-	stickClass: 'stickit',
-	endStickClass: 'stickit-end',
-	offset: 0,
-	onStick: null,
-	onUnstick: null
-});
+// var xxxx = document.querySelector('.xxxx');
+// var origOffsetY = header.offsetTop;
+
+// function onScroll(e) {
+//   window.scrollY >= origOffsetY ? header.classList.add('sticky') :
+//                                   header.classList.remove('sticky');
+// }
+
+// document.addEventListener('scroll', onScroll);
+
+//stick scroll part
+
+// $('.container').stickem({
+// 	item: '.stickem',
+// 	container: '.stickem-container',
+// 	stickClass: 'stickit',
+// 	endStickClass: 'stickit-end',
+// 	offset: 0,
+// 	onStick: null,
+// 	onUnstick: null
+// });
 
 (function($, window, document, undefined) {
 	var Stickem = function(elem, options) {
